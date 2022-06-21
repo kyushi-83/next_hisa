3bc72dc94ab75bfee941fd276c5741c020e40896
hisendou <hisendou@pasona.tech>
2022-06-21 14:15:05 +0900

    sss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
@@ -63,7 +63,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+
           </span>
         </a>
       </footer>
