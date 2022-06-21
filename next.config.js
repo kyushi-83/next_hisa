08ea5e88794ae1502a6c5cdf456642828450a331
hisendou <hisendou@pasona.tech>
2022-06-21 18:45:46 +0900

    json変更

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/repository_name" : "",
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
-
-// ESLintの設定 eslint-disable を追加する
-/* eslint-disable
-    @typescript-eslint/no-var-requires,
-    @typescript-eslint/explicit-function-return-type
-*/
+module.exports = {
+  // some configuration
+  assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : ""
+  // another configuration
+};
