30478c39c409eecd706e5847f23cac3373e9c935
hisendou <hisendou@pasona.tech>
2022-06-23 02:56:16 +0900

    sds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const urlPrefix = '/docs'
+//const urlPrefix = '/docs'
 
 module.exports = {
   assetPrefix: process.env.GITHUB_PAGES ? 'https://kyushi-83.github.io/next_hisa/' : '',
-  basePath: process.env.GITHUB_PAGES ? "https://kyushi-83.github.io/next_hisa/" : "",
+  basePath: process.env.GITHUB_PAGES ? 'https://kyushi-83.github.io/next_hisa/' : '',
   trailingSlash: true,
 }
