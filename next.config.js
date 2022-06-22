5cc2344f4a892edce8efc28565ba087464eaa365
hisendou <hisendou@pasona.tech>
2022-06-22 19:42:14 +0900

    sadaas

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   assetPrefix: isProd ? '/docs' : '',
-  basePath: process.env.GITHUB_ACTIONS ? "/repository_name" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/repository_name/docs" : "",
   trailingSlash: true
 };
