2a802512883291eafbbc9f0b9a50d694c1026b4b
hisendou <hisendou@pasona.tech>
2022-06-23 02:39:01 +0900

    sasasa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const urlPrefix = '/docs'
 
 module.exports = {
-  assetPrefix: process.env.GITHUB_PAGES ? '/repository_name' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? 'https://kyushi-83.github.io/next_hisa/' : '',
   basePath: process.env.GITHUB_PAGES ? "/repository_name/" : "",
   trailingSlash: true,
 }
