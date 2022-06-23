4622f51188f7d44d575f6798819a1b189eed8515
hisendou <hisendou@pasona.tech>
2022-06-23 19:16:03 +0900

    aadas

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default () =>(
   <div>
-    <Header />
     <Head>
       <title>list</title>
       <meta name="description" content="listページ" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
     <p>linklist page</p>
     <Link href="/">
       <a>back to top</a>
