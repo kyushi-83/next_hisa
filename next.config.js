8fc35a53b036b4ca5eb8d9e05a0ed4303baaa9e2
hisendou <hisendou@pasona.tech>
2022-06-23 14:30:04 +0900

    asdsada

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '/next_hisa' : ''
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix: prefixPath,
-  basePath: prefixPath,
-  reactStrictMode: true,
+module.exports = {
   trailingSlash: true,
+  assetPrefix: process.env.BASE_PATH || '',
+  basePath: process.env.BASE_PATH || '',
 }
-
-module.exports = nextConfig
